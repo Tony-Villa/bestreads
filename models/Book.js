e5b69eaff8dfc0d56fd72ba6e8b83379f62de343
tonyv <tonyvilla77@gmail.com>
2021-10-11 15:17:42 -0700

    added sass architecture

--- models/Book.js
+++ models/Book.js
@@ -1,31 +1,28 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        require: true,
-        unique: true
-    },
-    author: {
-        type: String,
-        require: true,
-        min: 1
-    },
-    genre: {
-        type: String, 
-        require: true
-    }, 
-    description: {
-        type: String,
-        require: true
-    },
-    coverImg: {
-        type: String,
-        require: true
-    },
+  title: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  author: {
+    type: String,
+    require: true,
+  },
+  genre: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  coverImg: {
+    type: String,
+    require: true,
+  },
 });
 
 const Books = mongoose.model('Books', bookSchema);
 module.exports = Books;
-
-
