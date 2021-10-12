510d3d46bf8889d682e9a93d1f510d293a5edeff
tonyv <tonyvilla77@gmail.com>
2021-10-12 16:15:28 -0700

    added shuffle algorithm for browse route

--- controllers/browse_controller.js
+++ controllers/browse_controller.js
@@ -1,32 +1,38 @@
 const router = require('express').Router();
 const { Book } = require('../models');
+const { shuffle } = require('../functions/shuffle');
 
+// Browse Route
 router.get('/', async (req, res) => {
- try {
-  const books = await Book.find({});
-  const context = {
-    books,
-  };
-  res.render('browse/collection.ejs', context);
- } catch (error) {
-   return console.log(error);
- } 
+  try {
+    const books = await Book.find({});
+
+    let shuffledBooks = shuffle(books);
+
+    const context = {
+      books: shuffledBooks,
+    };
+
+    res.render('browse/collection.ejs', context);
+  } catch (error) {
+    return console.log(error);
+  }
 });
 
-router.get('/:id', async (req,res, next) => {
+// Show Route
+router.get('/:id', async (req, res, next) => {
   try {
     const books = await Book.findById(req.params.id);
-    
+
     const context = {
       books,
-    }
-    return res.render('browse/show.ejs',context);
+    };
+    return res.render('browse/show.ejs', context);
   } catch (error) {
     console.log(error);
     req.error = error;
     return next();
-
-})
+  }
+});
 
 module.exports = router;
-
