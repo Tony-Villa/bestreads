bcd87735b92ed9a3ab372e5effae840201bf0819
Republic Union Trust <republicuniontrust@Walis-MacBook-Pro-2.local>
2021-10-12 14:35:16 -0700

    create show route

--- controllers/browse_controller.js
+++ controllers/browse_controller.js
@@ -2,15 +2,31 @@ const router = require('express').Router();
 const { Book } = require('../models');
 
 router.get('/', async (req, res) => {
-  try {
-    const books = await Book.find({});
-    const context = {
-      books,
-    };
-    res.render('browse/collection.ejs', context);
-  } catch (error) {
-    return console.log(error);
-  }
+ try {
+  const books = await Book.find({});
+  const context = {
+    books,
+  };
+  res.render('browse/collection.ejs', context);
+ } catch (error) {
+   return console.log(error);
+ } 
 });
 
+router.get('/:id', async (req,res, next) => {
+  try {
+    const books = await Book.findById(req.params.id);
+    
+    const context = {
+      books,
+    }
+    return res.render('browse/show.ejs',context);
+  } catch (error) {
+    console.log(error);
+    req.error = error;
+    return next();
+
+})
+
 module.exports = router;
+
