c227f04d0537e0ed91d6cb777127b277b48233df
tonyv <tonyvilla77@gmail.com>
2021-10-12 19:43:17 -0700

    added hashing for passwords

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 const { User } = require('../models');
 const { registerValidation, loginValidation } = require('../functions/validation');
 
@@ -9,13 +10,21 @@ router.get('/register', (req, res) => {
 router.post('/register', async (req, res) => {
   //Validate data
   const { error } = registerValidation(req.body);
-
   if (error) return res.status(400).send(error.details[0].message);
 
+  // Check if user is already in DB
+  const emailExists = await User.findOne({ email: req.body.email });
+  if (emailExists) return res.status(400).send('Email already exists');
+
+  // Hash passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+  // Create mew user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPass,
   });
 
   try {
