54215d0157cc57af64178f9b0bde1373c1443da4
tonyv <tonyvilla77@gmail.com>
2021-10-14 11:31:51 -0700

    added buy book functionality

--- public/scripts/collection.js
+++ public/scripts/collection.js
@@ -1,8 +1,24 @@
+// Text Content
 const $bookAbridgeDesc = $('.abridged-conditional');
 const $fullDesc = $('.full-desc');
+const $bookTitle = $('.get-book-title').text();
+
+// Links
+const $buyRedirect = $('.buy-confirm-link');
+
+// Buttons
 const $moreBtn = $('.expand-desc');
 const $lessBtn = $('.less-desc');
+const $buyBtn = $('.buy-btn');
 
+// Global Variables
+
+// Functions
+const urlNameMatch = (str) => {
+  return str.toLowerCase().split(' ').join('%20');
+};
+
+// Event Listeners
 $moreBtn.click(() => {
   $bookAbridgeDesc.addClass('hidden');
   $fullDesc.removeClass('hidden');
@@ -11,4 +27,8 @@ $moreBtn.click(() => {
 $lessBtn.click(() => {
   $bookAbridgeDesc.removeClass('hidden');
   $fullDesc.addClass('hidden');
+});
+
+$buyBtn.click(() => {
+  $buyRedirect.attr('href', `https://www.esowonbookstore.com/search/site/${urlNameMatch($bookTitle)}`);
 });
