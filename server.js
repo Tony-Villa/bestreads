7eeef1b815d68eca00bbb642d9deaae76e710bed
Republic Union Trust <republicuniontrust@Walis-MacBook-Pro-2.local>
2021-10-14 17:22:20 -0700

    print log

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app = express();
 app.set('view engine', 'ejs');
 // app.use(express.static('public'));
 app.use(express.static(__dirname));
-
+// Print dir name
+console.log('this dirname:',__dirname);
 // URL Parser Middleware && Method Overide
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
