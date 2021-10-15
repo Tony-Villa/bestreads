d55b34f9c90913297b747a4aaf7dfc7a26878b4d
tonyv <tonyvilla77@gmail.com>
2021-10-14 23:17:23 -0700

    removed console.log from app script

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,17 @@
-console.log('I am connected');
+// console.log('I am connected');
+
+/*
+  <script type="text/javascript">
+    const error = '<%= error.details[0].message%>';
+
+    console.log(error);
+
+    const showAlert = function (err) {
+      alert('error: ' + err);
+    };
+
+    if (error !== null) {
+      showAlert(error);
+    }
+  </script>
+  */
